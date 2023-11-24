a88662ac27d299509d2c65b3b9c04feb32d90790
Sherlock67 <alammushfiqul366@gmail.com>
2023-11-24 16:04:17 +0600

    Domain Project added

---
+++ webapi/SmartTaskMgt.Domain/Entities/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTaskMgt.Domain.Entities
+{
+    public class Task
+    {
+        public Task()
+        {
+            
+        }
+        public Guid TaskId { get; set; }
+        public string TaskName { get; set; }
+    }
+}
